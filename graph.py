7e6040d5f9695148514a0e98c118e263d8617700
naibla8358 <naibla8358@gmail.com>
2015-11-28 00:27:38 -0500

    Redo pretty much everything

--- graph.py
+++ graph.py
@@ -1,5 +1,129 @@
 import numpy as np
 from graphstorage import MatrixStorage
+from abc import ABCMeta, abstractmethod
+
+class GraphInterface(object):
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def GetNode(self, nodeIndex): raise NotImplementedError
+
+    @abstractmethod
+    def AddNode(self): raise NotImplementedError
+
+    @abstractmethod
+    def RemoveNode(self, node): raise NotImplementedError
+
+    @abstractmethod
+    def GetWeight(self, node1, node2): raise NotImplementedError
+
+    @abstractmethod
+    def SetWeight(self, node1, node2, val): raise NotImplementedError
+
+    @abstractmethod
+    def GetData(self, node): raise NotImplementedError
+
+    @abstractmethod
+    def SetData(self, node, val): raise NotImplementedError
+
+    @abstractmethod
+    def __getitem__(self, indices): raise NotImplementedError
+
+    @abstractmethod
+    def __len__(self): raise NotImplementedError
+
+
+
+
+
+class Graph(GraphInterface):
+    def __init__(self, numnodes, directed=False, 
+            storage=MatrixStorage):
+        self.directed = directed
+        self.storage = storage(self.N)
+        self.data = [ None for i in range(self.N) ]
+        self._unset = set()
+
+    ###### Interface Methods #####
+
+    def GetNode(self, nodeIndex):
+        if nodeIndex is None:
+            raise TypeError("node id cannot be None")
+        if not isinstance(nodeIndex, int):
+            raise TypeError("node id must be an int")
+        self._IdGuard(nodeIndex)
+        return _Node(self, nodeIndex)
+
+    def AddNode(self):
+        if len(self._unset) > 0:
+            nid = self._unset.pop()
+            self.storage.Zero(nid)
+        else:
+            nid = self.storage.AddNode()
+            self.data.append(None)
+
+        return self.GetNode(nid)
+
+    def RemoveNode(self, node):
+        nid = self._NodeId(node)
+        self._IdGuard(nid)
+
+        if nid == len(self.data) - 1:
+            self.data.pop()
+            self.storage.Remove()
+        else:
+            self.data[nid] = None
+            self._unset.add(nid)
+
+    def GetWeight(self, node1, node2):
+        nid1 = self._NodeId(node1)
+        nid2 = self._NodeId(node1)
+        self._IdGuard(nid1)
+        self._IdGuard(nid2)
+
+        return self.storage.Get(nid1, nid2)
+
+    def SetWeight(self, node1, node2, val):
+        nid1 = self._NodeId(node1)
+        nid2 = self._NodeId(node1)
+        self._IdGuard(nid1)
+        self._IdGuard(nid2)
+
+        return self.storage.Set(nid1, nid2, val)
+
+    def GetData(self, node):
+        nid = self._NodeId(node)
+        self._IdGuard(nid)
+        return self.data[nid]
+
+    def SetData(self, node, val):
+        nid = self._NodeId(node)
+        self._IdGuard(nid)
+        self.data[nid] = val
+
+    def __getitem__(self, indices):
+        return self.GetNode(indices)
+
+    def __len__(self):
+        return len(self.storage) - len(self._unset)
+
+    ###### Custom Methods #####
+
+    def _NodeId(node):
+        if node is None:
+            raise TypeError("Node cannot be None")
+        if isinstance(node, _Node):
+            return node.nid
+        if not isinstance(node, int):
+            return int(node)
+        return node
+
+    def _IdGuard(nid):
+        if nid not in self.storage or nid in self._unset:
+            raise KeyError("Invalid node id: %s" % nid)
+
+
+
 
 def _ensureStartId(startNode):
     if isinstance(startNode, _Node):
@@ -9,7 +133,7 @@ def _ensureStartId(startNode):
     return startNode
 
 
-class Graph(object):
+class Graph2(object):
     def __init__(self, numnodes, directed=False, 
             storage=MatrixStorage):
         self.N = numnodes
@@ -72,7 +196,50 @@ class Graph(object):
         return len(self.storage)
 
 
+
+
 class _Node(object):
+    def __init__(self, graph, nid):
+        if not isinstance(graph, GraphInterface):
+            raise TypeError("Invalid graph: " % graph)
+        self.graph = graph
+        self.nid = nid
+
+    def Connect(self, node, weight):
+        return self.graph.SetWeight(self.nid, node, weight)
+
+    def Weight(self, node):
+        return self.graph.GetWeight(self.nid, nodeId)
+
+    def Children(self):
+        ids = self.graph.storage.GetChildren(self.nid)
+        return [ self.graph[nid] for nid in ids ]
+
+    def ChildIds(self):
+        ids = self.graph.storage.GetChildren(self.nid)
+        return list(ids)
+
+    def Parents(self):
+        ids = self.graph.storage.GetParents(self.nid)
+        return [ self.graph[nid] for nid in ids ]
+
+    def ParentIds(self):
+        ids = self.graph.storage.GetParents(self.nid)
+        return list(ids)
+
+    def Get(self):
+        return self.graph.GetData(self.nid)
+
+    def Set(self, val):
+        return self.graph.SetData(self.nid, val)
+
+    def __getitem__(self, nid):
+        return self.Weight(nid)
+
+
+
+
+class _Node2(object):
     def __init__(self, graph, nid):
         self.graph = graph
         self.nid = nid
--- graphstorage.py
+++ graphstorage.py
@@ -1,9 +1,182 @@
 import numpy as np
+from abc import ABCMeta, abstractmethod
 
-class MatrixStorage(object):
+
+class StorageBase(object):
+    __metaclass__ = ABCMeta
+
+    def __init__(self, N):
+        if not isinstance(N, int):
+            raise TypeError("Invalid N: %s" % N)
+        self.N = N
+
+    @abstractmethod
+    def Get(self, n1, n2): raise NotImplementedError
+
+    @abstractmethod
+    def Set(self, n1, n2): raise NotImplementedError
+
+    @abstractmethod
+    def AddNode(self): raise NotImplementedError
+
+    @abstractmethod
+    def Remove(self): raise NotImplementedError
+
+    @abstractmethod
+    def Zero(self, nid): raise NotImplementedError
+
+    @abstractmethod
+    def Children(self, nid): raise NotImplementedError
+
+    @abstractmethod
+    def Parents(self, nid): raise NotImplementedError
+
+    def __len__(self):
+        return self.N
+
+    def __getitem__(self, indices):
+        start, end = self._ParseIndices(indices)
+        return self.Get(start, end)
+
+    def __setitem__(self, indices, val):
+        start, end = self._ParseIndices(indices)
+        return self.Set(start, end, val)
+
+    def __contains__(self, nid):
+        if not isinstance(nid, int):
+            raise TypeError("Invalid node id type: %s" % type(nid))
+        return nid >= 0 and nid < self.N
+
+    def _IdGuard(self, *args):
+        for arg in args:
+            _IdGuardSingle(arg)
+
+    def _IdGuardSingle(self, nid):
+        if not isinstance(nid, int):
+            raise TypeError("Invalid node id type: %s" % type(nid))
+        if nid < 0 or nid >= self.N:
+            raise KeyError("Invalid node id: %s" % nid)
+
+    def _ParseIndices(self, indices):
+        if isinstance(indices, int):
+            raise IndexError("There must be 2 indices given")
+
+        if isinstance(indices, tuple):
+            if len(indices) != 2:
+                raise IndexError("There must be 2 indices given")
+            return indices
+
+        if isinstance(indices, slice):
+            return (indices.start, indices.finish)
+
+        raise TypeError("Invalid indexing type given: %s" % type(nid))
+
+
+
+
+
+class MatrixStorage(StorageBase):
+    def __init__(self, N):
+        super(MatrixStorage, self).__init__(N)
+        self.matr = np.zeros((N, N))
+
+    def Get(self, n1, n2):
+        self._IdGuard(n1, n2)
+        return self.matr[n1, n2]
+
+    def Set(self, n1, n2, val):
+        self._IdGuard(n1, n2)
+        self.matr[n1, n2] = val
+
+    def AddNode(self):
+        newN = self.N + 1
+        newMatr = np.zeros((newN, newN))
+        newMatr[:self.N, :self.N] = self.matr
+
+        self.N = newN
+        self.matr = newMatr
+
+    def Remove(self):
+        self.N -= 1
+        self.matr = self.matr[:self.N, :self.N]
+
+    def Zero(self, nid):
+        self._IdGuard(nid)
+        self.matr[nid,:] = 0
+        self.matr[:, nid] = 0
+
+    def Children(self, nid):
+        self._IdGuard(nid)
+        return (i for i, x in enumerate(self.matr[nid, :]) if x != 0)
+
+    def Parents(self, nid):
+        self._IdGuard(nid)
+        return (i for i, x in enumerate(self.matr[:, nid]) if x != 0)
+
+    def __len__(self):
+        assert(self.matr.shape[0] == self.N)
+        return self.N
+
+
+
+
+
+class ListStorage(StorageBase):
+    def __init__(self, N):
+        super(MatrixStorage, self).__init__(N)
+        self.data = [ {} for i in range(N) ]
+
+    def Get(self, n1, n2):
+        self._IdGuard(n1, n2)
+        if n2 not in self.data[n1]:
+            return 0
+        return self.data[n1][n2]
+
+    def Set(self, n1, n2, val):
+        self._IdGuard(n1, n2)
+        self.data[n1][n2] = val
+
+    def AddNode(self):
+        self.data.append({})
+        self.N += 1
+        assert(len(self.data) == self.N)
+
+    def Remove(self):
+        self._Cleanse(nid)
+        self.N -= 1
+        self.data.pop()
+        assert(len(self.data) == self.N)
+
+    def Zero(self, nid):
+        self._IdGuard(nid)
+        self.data[nid] = {}
+        self._Cleanse(nid)
+
+    def Children(self, nid):
+        self._IdGuard(nid)
+        return iter(self.data[nid])
+
+    def Parents(self, nid):
+        self._IdGuard(nid)
+        return (index for index, x in enumerate(self.data) if nid in x)
+
+    def __len__(self):
+        assert(len(self.data) == self.N)
+        return self.N
+
+    def _Cleanse(self, nid):
+        for i in range(self.N):
+            if nid in self.data[i]:
+                del self.data[i][nid]
+
+
+
+
+
+class MatrixStorage2(object):
     def __init__(self, N, directed):
         if not isinstance(N, int):
-            raise Exception("Invalid first parameter")
+            raise TypeError("Invalid first parameter")
         self.N = N
         self.matr = np.zeros((self.N, self.N))
         self._unset = set()
@@ -91,7 +264,7 @@ class MatrixStorage(object):
 
 
 
-class ListStorage(object):
+class ListStorage2(object):
     def __init__(self, N, directed):
         self.data = [ {} for i in range(N) ]
         self.directed = directed
